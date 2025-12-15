0b6f3a6c9a6554084b2b86907e4675be6637f2a7
karunkumarkoduri <24331a05d5@mvgrce.edu.in>
2025-12-16 01:58:53 +0530

    Add files via upload

---
+++ 1datatype.py
@@ -0,0 +1,7 @@
+a=10
+b='k'
+c=10.11
+d='karun'
+print("int=",a,"\ncharecter=",b,"\nfloating point=",c,"\nstring=",d)
+print("Types:")
+print("a=",type(a),"\nb=",type(b),"\nc=",type(c),"\nd=",type(d))
