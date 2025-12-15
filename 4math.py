dfd989e947ad6375c451f7f5dbd7632d4c34e472
karunkumarkoduri <24331a05d5@mvgrce.edu.in>
2025-12-16 02:04:58 +0530

    Add files via upload

---
+++ 4math.py
@@ -0,0 +1,8 @@
+import math
+print("Value of pi:", math.pi)
+a=4
+print("Square root of",a,"is",math.sqrt(a))
+angle = 60
+print("Cosine of 60 degrees:", math.cos(angle))
+angle = 90
+print("Sine of 90 degrees:", math.sin(angle))
