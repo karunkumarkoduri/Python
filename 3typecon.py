634e3a0477b496125deba664cd555165b5b2ce1c
karunkumarkoduri <24331a05d5@mvgrce.edu.in>
2025-12-16 02:02:37 +0530

    Add files via upload

---
+++ 3typecon.py
@@ -0,0 +1,10 @@
+a=10
+b=20.22
+c=b/a
+print("a=",a," b=",b," c=b/a=",c)
+print("type of a=",type(a))
+print("type of b=",type(b))
+print("implicit type conversion (b/a)=",type(c))
+d=int(c)
+print("explicit type conversion of b/a to int=",d)
+print("type of b/a=",type(d))
