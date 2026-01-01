9e6a1998d5e3aebf6ca83bc82567324d588a4999
karunkumarkoduri <24331a05d5@mvgrce.edu.in>
2026-01-01 18:30:04 +0530

    Add files via upload

---
+++ evod.py
@@ -0,0 +1,10 @@
+a=int(input("Enter range:"))
+count1=0
+count2=0
+for i in range(1,a+1):
+    if(i%2 == 0):
+        count1 = count1 + 1
+    else:
+        count2 = count2 + 1
+print("number of Evens:",count1)
+print("number of odds:",count2)
