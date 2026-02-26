ef9d807218dfd15cfd5574af26b03afd1752e197
karunkumarkoduri <24331a05d5@mvgrce.edu.in>
2026-02-27 04:27:51 +0530

    Add files via upload

---
+++ 4updatedict.py
@@ -0,0 +1,6 @@
+print("---Updating dictionary---")
+dict={"name":"kkk","dept":"CSE","year":2}
+dict["sem"]=1
+print("After adding new values:",dict)
+dict["sem"]=2
+print("After updating data:",dict)
