033b9639ec131250f928d744dd3f6c199056c54f
karunkumarkoduri <24331a05d5@mvgrce.edu.in>
2026-02-27 04:26:37 +0530

    Add files via upload

---
+++ 3dictionaries.py
@@ -0,0 +1,9 @@
+print("---Implementing the Dictionary methods---")
+dict={1:"a",2:"b",3:"c",4:"d",5:"e"}
+print("dictionary:",dict)
+print("a)Keys:",dict.keys())
+print("b)Values:",dict.values())
+print("c)Items:",dict.items())
+print("d)pop:(3)",dict.pop(3))
+del dict[4]
+print("e)Delete:",dict)
